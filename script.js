26c994a92373531ca34ab1e6ed6b2544905753af
arsyhiy <arsyhiy32@gmail.com>
2025-08-27 14:25:43 +0300

    update no comment

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 /*
-const display = document.getElementById('text-display');
-const wpmEl = document.getElementById('wpm');
-const accuracyEl = document.getElementById('accuracy');
-const restartBtn = document.getElementById('restart-btn');
 
-const sampleText = 'we need to replace this'; // i need to delete this and replace with generateText function
+//const display = document.getElementById('text-display');
+//const sampleText = 'we need to replace this'; // i need to delete this and replace with generateText function
 
-let timer = 60; // i need to make it choiceable and not only time and by words
+
 let interval;
 let started = false;
 
-function startTimer() {
-  interval = setInterval(() => {
-    timer--;
-    timerEl.textContent = timer;
-    if (timer <= 0) {
-      clearInterval(interval);
-      input.disabled = true;
-      updateStats();
-    }
-  }, 1000);
-}
 
-// startTimer analog but not by times but by number of words
-function startwords(){
 
-}
 
 function updateStats() {
   const typed = input.value;
@@ -35,29 +18,6 @@ function updateStats() {
   wpmEl.textContent = wpm;
   accuracyEl.textContent = `${accuracy}%`;
 }
-
-function resetTest() {
-  // rewrite this func
-  input.value = '';
-  input.disabled = false;
-  input.focus();
-  display.textContent = sampleText;
-  timer = 60;
-  timerEl.textContent = timer;
-  wpmEl.textContent = 0;
-accuracyEl.textContent = '100%';
-  clearInterval(interval);
-  started = false;
-}
-
-input.addEventListener('input', () => {
-    // rewrite this func
-  if (!started) {
-    startTimer();
-    started = true;
-  }
-  updateStats();
-});
 
 restartBtn.addEventListener('click', resetTest);
 
@@ -77,11 +37,69 @@ function generateText(){
 
 
 
+const words = `find time line day seem here increase house may know of that with never again and come order what all want between more number show year same keep stand since good who long see so interest these use take much they work down home in people person go without way about through early where have present program`.split(' ');
+//let words = []
+let currentWordIndex = 0;
+let startTime = null;
+let totalTyped = 0;
+let correctTyped = 0;
+
+// curently no in index.html
+let timer = 60; // i need to make it choiceable and not only time and by words
+
+const input = document.getElementById('hidden-input');
+const wpmEl = document.getElementById('wpm');
+const accuracyEl = document.getElementById('accuracy');
+const container = document.getElementById('word-container');
+
+// curently no in index.html
+//const restartBtn = document.getElementById('restart-btn'); 
+
+const timerEl = document.getElementById('timer'); 
 
 
 
-/*
-//const words = `find time line day seem here increase house may know of that with never again and come order what all want between more number show year same keep stand since good who long see so interest these use take much they work down home in people person go without way about through early where have present program`.split(' ');
+async function loadwords() {
+  try {
+    const response = await fetch('words.json');
+    if (!response.ok) {
+      throw new Error('Ошибка загрузки файла');
+    }
+    const data = await response.json();
+    words = data.words; // предполагается, что в JSON есть поле "words"
+    console.log('Слова загружены:', words);
+  } catch (error) {
+    console.error('Ошибка при загрузке или парсинге:', error);
+  }
+}
+
+//loadwords()
+
+
+function startTimer() {
+  interval = setInterval(() => {
+    timer--;
+    timerEl.textContent = timer;
+    if (timer <= 0) {
+      clearInterval(interval);
+      input.disabled = true;
+      updateStats();
+    }
+  }, 1000);
+}
+
+// startTimer analog but not by times but by number of words
+function startwords(){
+
+}
+
+
+
+
+
+
+
+
 
 function renderWords() {
   container.innerHTML = '';
@@ -164,6 +182,13 @@ function updateStats() {
   accuracyEl.textContent = `${accuracy}%`;
 }
 
+
+
+
+
+
+
+
 input.addEventListener('keydown', (e) => {
   if (!startTime) startTime = Date.now();
 
@@ -179,78 +204,9 @@ input.addEventListener('input', () => {
   updateWordDisplay();
 });
 
-renderWords();
-highlightCursor();
-*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let words = []
-let currentWordIndex = 0;
-let startTime = null;
-let totalTyped = 0;
-let correctTyped = 0;
-
-
-const input = document.getelementbyid('hidden-input');
-const wpmEl = document.getElementById('wpm');
-const accuracyEl = document.getElementById('accuracy');
-const container = document.getelementbyid('word-container');
-
-
-/* const timerEl = document.getElementById('timer'); we need to get this thing back
-
-*/
-
-async function loadwords() {
-  try {
-    const response = await fetch('words.json');
-    if (!response.ok) {
-      throw new Error('Ошибка загрузки файла');
-    }
-    const data = await response.json();
-    words = data.words; // предполагается, что в JSON есть поле "words"
-    console.log('Слова загружены:', words);
-  } catch (error) {
-    console.error('Ошибка при загрузке или парсинге:', error);
-  }
-}
-
-loadwords()
-
-
-
+renderWords()
+highlightCursor()
 
 
 /*
@@ -259,4 +215,38 @@ loadwords()
 
  */
 
+// ============================== все ниже это уже чистый вариант
 
+
+
+
+
+
+
+
+// event listeners section
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === "Escape") {
+    console.log("escape key is pressed..")
+    resetTest()
+  }
+});
+
+
+
+// function section
+
+function resetTest() {
+  // rewrite this func
+  input.value = '';
+  input.disabled = false;
+  input.focus();
+  display.textContent = sampleText; // rewrite 
+  timer = 60; // rewrite
+  timerEl.textContent = timer;
+  wpmEl.textContent = 0;
+accuracyEl.textContent = '100%';
+  clearInterval(interval);
+  started = false;
+}
